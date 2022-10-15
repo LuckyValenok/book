41e8a89a1d9fc9b60b43678afbc3d8fa8f6c1cac
Vlad <37305073+LuckyValenok@users.noreply.github.com>
2022-10-15 08:27:48 -0700

    Использование params заменено на аргументы в методах

--- grails-app/controllers/book/BookController.groovy
+++ grails-app/controllers/book/BookController.groovy
@@ -5,29 +5,19 @@ class BookController {
 
     private final Map<Integer, Book> books = new HashMap<>()
 
-    BookController() {
-        books.put(1, new Book(1, 'Test1', 'Test1'))
-        books.put(2, new Book(2, 'Test2', 'Test2'))
-    }
-
     def list() {
         render view: 'list', model: [books: books.values()]
     }
 
-    def delete() {
-        if (params.id != null) {
-            try {
-                books.remove(Integer.parseInt(params.id))
-            } catch (NumberFormatException ignored) {
-            }
-        }
+    def delete(int id) {
+        books.remove(id)
         redirect(action: 'list')
     }
 
-    def save() {
-        if (params.title != null && params.author != null) {
+    def save(String title, String author) {
+        if (title != null && author != null) {
             int id = books.size() + 1
-            books.put(id, new Book(id, params.title, params.author))
+            books.put(id, new Book(id, title, author))
         }
 
         redirect(action: 'list')
